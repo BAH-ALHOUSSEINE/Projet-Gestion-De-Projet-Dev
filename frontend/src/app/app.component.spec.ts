de296ac18bac126af69898f2cfbee563c7201bf6
Odrian Tarrieu <odriantarrieu@MacBook-Pro-de-Odrian.local>
2024-12-02 11:38:19 +0100

    ajout test app.component

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,35 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MatDialog } from '@angular/material/dialog';
+import { HeaderComponent } from './header/header.component';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let appComponent: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let dialog: jasmine.SpyObj<MatDialog>;
+
   beforeEach(async () => {
+    const dialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
+
     await TestBed.configureTestingModule({
       imports: [
         RouterModule.forRoot([])
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
       ],
+      providers: [
+        { providers: MatDialog, useValue: dialogSpy }
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    appComponent = fixture.componentInstance;
+    dialog = TestBed.inject(MatDialog) as jasmine.SpyObj<MatDialog>;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
+    expect(appComponent).toBeTruthy();
   });
+
+  // it('should call dialog.open when openPopop is called', () => {
+  //   appComponent.openPopup();
+  //   expect(dialog.open).toHaveBeenCalled();
+  // });
 });
